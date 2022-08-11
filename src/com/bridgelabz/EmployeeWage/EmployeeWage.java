16b09c2b46c4328b646f7c40392be12406906356
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-11 13:02:31 +0530

    [Prem]:Added monthly wage till total working hour

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -19,7 +19,12 @@ public class EmployeeWage
 				case 0:
 					System.out.println("Employee is present");
 					System.out.println("Employee wage=" +fullDayHour*wagePerHour);
-					System.out.println("Employee monthly wage=" +fullDayHour*wagePerHour*workingDayPerMonth);
+					int totalHourPerMonth=1;
+					while(totalHourPerMonth<=100)
+					{
+						totalHourPerMonth++;
+					}
+					System.out.println("Employee monthly wage=" +totalHourPerMonth*wagePerHour);
 					break;
 					
 				case 1:
@@ -31,7 +36,10 @@ public class EmployeeWage
 				case 2:
 					System.out.println("Employee is absent");
 					System.out.println("Employee wage=0");
-					break;	
+					break;
+					
+					
 				}
-			}
+
+		}
 }
